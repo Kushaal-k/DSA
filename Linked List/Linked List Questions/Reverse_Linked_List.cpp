21cfb3aa51673b46b999453b01ca5614e277fed3
Kushaal Kankane <kushaalkankane12345@gmail.com>
2024-07-20 00:35:39 +0530

    Linked List Question

---
+++ Linked List/Linked List Questions/Reverse_Linked_List.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+class ListNode{
+    public :
+    int data;
+    ListNode* next;
+
+
+    // Constructor
+    ListNode(int data){
+        this -> data = data;
+        this-> next = NULL;
+    }
+};
+
+
+class Solution {
+private:
+    void reverse(ListNode* &head,ListNode* curr,ListNode* prev){
+
+        if(curr== NULL){
+            head = prev;
+            return;
+        }
+
+        ListNode* forward = curr -> next;
+        reverse(head,forward,curr);
+        curr->next = prev;
+    }
+public:
+    ListNode* reverseList(ListNode* head) {
+
+
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        reverse(head,curr,prev);
+        return head;
+        
+        // if(head==NULL || head->next == NULL){
+        //     return head;
+        // }
+
+        // ListNode* prev = NULL;
+        // ListNode* curr = head;
+        // ListNode* forward = NULL;
+
+        // while(curr!=NULL){
+        //     forward = curr->next;
+        //     curr ->next = prev;
+        //     prev = curr;
+        //     curr = forward;
+        // }
+        
+        // return prev;
+    }
+};
+
+void print(ListNode* head){
+    ListNode* temp = head;
+
+    while(temp!=NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main(){
+
+    ListNode* head = NULL;
+
+    ListNode* reverseList(head);
+
+    return 0;
+}
